69b9738910dcfca53d33d1a0ef5508ffc80bfb07
Vince <vincent@treehouse.finance>
2021-10-26 22:06:06 +0800

    renamed the file

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,25 @@
+def Individual():
+    return
+
+def Population():
+    return
+
+
+def Crossover():
+    return
+
+
+def Mutation():
+    return
+
+
+def Fitness():
+    return
+
+
+def Selection():
+    return
+
+
+def GenericAlgorithm():
+    return
