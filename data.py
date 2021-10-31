1cfcedc5532efb76556f0ca489f5226663be3f03
Vince <vincent@treehouse.finance>
2021-11-01 00:47:56 +0800

    added some data files

--- data.py
+++ data.py
@@ -4,8 +4,8 @@ import numpy as np
 PRICE_DF = pd.read_csv('prediction.csv')
 STOCK_ARR = np.array(PRICE_DF.columns)
 
-RET_DF = PRICE_DF.diff().dropna()
+RET_DF = PRICE_DF.diff().dropna()/PRICE_DF.iloc[0:-1,:]
 RET_DF_10 = RET_DF.iloc[:,0:10]
-# RET_DF = PRICE_DF.diff().loc[1:,:].div(PRICE_DF.diff().loc[0:-1,:],axis=1)
+
 
 
