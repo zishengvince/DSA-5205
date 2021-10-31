a80c64eb90cd5655ad34dd5865ba6631021c73b1
Vince <vincent@treehouse.finance>
2021-10-31 21:47:01 +0800

    added some lines in main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import GeneticAlgorithm
+import pandas as pd
+import numpy as np
+import GeneticAlgorithm as ga
 import LSTM
 import SVM
 import MarkowitzEF
-
+import data
 
 
 
 if __name__ == "__main__":
-    pass
+    best = ga.gaStockSelection()
+    gene = best.gene
+    gene_arr = ga.cutText(gene)
+    weight_arr = np.array([ga.decodeGene(sub_gene) for sub_gene in gene_arr])
+    max_index_list = ga.getMaxNIndex(weight_arr)
+    weight_arr = weight_arr[max_index_list]
+    weight_arr /= sum(weight_arr)
+    stock_arr = data.STOCK_ARR[max_index_list]
+    for i in range(len(stock_arr)):
+        print((stock_arr[i],weight_arr[i]))
