be1eba3ab4d8229e558158edbbfbad157cd44524
Vince <vincent@treehouse.finance>
2021-10-31 21:49:05 +0800

    added params.py

---
+++ params.py
@@ -0,0 +1,8 @@
+# gene: 100 stocks * weight code for each stock = 100 * 5
+stock_gene_len = 5
+stock_count = 100
+cross_rate = 0.4
+mutation_rate = 0.4
+unit_amount = 1000
+gene_length = stock_count * stock_gene_len
+generation = 10
